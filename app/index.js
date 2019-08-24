5592e0017af0c90b483bc3ccbb5618f6e0d25f14
Shawn Axsom <axs221@gmail.com>
2019-08-24 16:25:55 -0400

    Separate Watch mode. Stopwatch for Exercise mode.

--- app/index.js
+++ app/index.js
@@ -4,15 +4,59 @@ import { preferences } from "user-settings";
 import * as util from "../common/utils";
 import userActivity from "user-activity";
 import { HeartRateSensor } from "heart-rate";
+import { display } from "display";
 
-// Update the clock every second
+const month = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+const weekday = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+
 clock.granularity = "seconds";
 
-// Get a handle on the <text> element
+const dayLabel = document.getElementById("dayLabel");
+const dateLabel = document.getElementById("dateLabel");
 const timeLabel = document.getElementById("timeLabel");
 
 let stepsAtStartOfHour = 0;
 let thisHour = 0;
+let stopwatchTimeStart = null;
+
+const show = elementName => {
+  const el = document.getElementById(elementName);
+  el.style.display = "inline";
+};
+
+const hide = elementName => {
+  const el = document.getElementById(elementName);
+  el.style.display = "none";
+};
+
+const drawDay = date => {
+  dayLabel.text = weekday[date.getDay()];
+};
+
+const drawDate = date => {
+  dateLabel.text = `${month[date.getMonth()]} ${date.getDate()}`;
+};
 
 const drawTime = date => {
   const today = date;
@@ -26,6 +70,20 @@ const drawTime = date => {
   }
   let mins = util.zeroPad(today.getMinutes());
   timeLabel.text = `${hours}:${mins}`;
+};
+
+function padZero(number, size) {
+  number = number.toString();
+  while (number.length < size) number = "0" + number;
+  return number;
+}
+
+const drawStopWatch = date => {
+  const diffSeconds = Math.abs(date - stopwatchTimeStart) / 1000;
+  const minutes = Math.floor(diffSeconds / 60);
+  const seconds = Math.floor(diffSeconds % 60);
+
+  timeLabel.text = `${padZero(minutes, 2)}:${padZero(seconds, 2)}`;
 };
 
 const drawTotalSteps = () => {
@@ -112,6 +170,17 @@ function toHex(d) {
 //   return `${toHex(255 - value)}${toHex(value)}20`;
 // };
 
+let lastTick = null;
+let inExerciseMode = false;
+
+const background = document.getElementById("background");
+background.addEventListener("click", evt => {
+  inExerciseMode = !inExerciseMode;
+  stopwatchTimeStart = lastTick.date;
+
+  handleTick(lastTick);
+});
+
 const getBackgroundColor = today => {
   const hour = today.getHours();
   const minute = today.getMinutes();
@@ -137,13 +206,73 @@ const getBackgroundColor = today => {
   return `#${toHex(225 - value)}${toHex(value)}20`;
 };
 
-clock.ontick = evt => {
-  const background = document.getElementById("background-color");
-  background.style.fill = getBackgroundColor(evt.date);
+function handleExerciseModeTick(evt) {
+  const backgroundColor = document.getElementById("background-color");
+  backgroundColor.style.fill = getBackgroundColor(evt.date);
 
+  hide("dateLabel");
+  hide("dayLabel");
+  show("totalStepsLabel");
+  show("bottomLine");
+  show("totalStepsLabel");
+  show("heartRateLabel");
+  show("heartRateIcon");
+  show("hourlyStepsLabel");
+  show("trajectoryStepsLabel");
+  show("topLine");
+
+  drawStopWatch(evt.date);
+  drawTotalSteps();
+  drawHourlySteps(evt.date);
+  drawTrajectorySteps(evt.date);
+  drawHeartRate();
+
+  display.autoOff = false;
+  display.on = true;
+  display.brightnessOverride = 1.0;
+
+  return;
+}
+
+function handleClockModeTick(evt) {
+  const backgroundColor = document.getElementById("background-color");
+  backgroundColor.style.fill = getBackgroundColor(evt.date);
+
+  show("dateLabel");
+  show("dayLabel");
+  hide("bottomLine");
+  hide("totalStepsLabel");
+  hide("heartRateLabel");
+  hide("heartRateIcon");
+  hide("hourlyStepsLabel");
+  hide("trajectoryStepsLabel");
+  hide("topLine");
+
+  drawDay(evt.date);
+  drawDate(evt.date);
   drawTime(evt.date);
   drawTotalSteps();
   drawHourlySteps(evt.date);
   drawTrajectorySteps(evt.date);
   drawHeartRate();
+
+  display.autoOff = false;
+  display.on = true;
+  display.brightnessOverride = 1.0;
+
+  return;
+}
+
+const handleTick = evt => {
+  if (inExerciseMode) {
+    handleExerciseModeTick(evt);
+  } else {
+    handleClockModeTick(evt);
+  }
+};
+
+clock.ontick = evt => {
+  lastTick = evt;
+
+  handleTick(evt);
 };
